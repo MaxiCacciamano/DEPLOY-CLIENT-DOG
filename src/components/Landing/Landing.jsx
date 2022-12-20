801f48745c37c8b8ef9a4331e050bdd1c37ad265
MaxiCacciamano <maximilianocacciamanomail.com>
2022-12-19 23:19:56 -0300

    commit3

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 import style from './Landing.module.css'
-import img from '../../img/a.jpg'
+import logo from '../../img/depositphotos_180617216-stock-illustration-german-shepherd-dog-isolated-outlined (1).jpg'
+
 export const Landing = () => {
   return (
     <div className={style.landing}>
-        <h1>Bienvenidos</h1>
-        <h2>Food Api Dogs</h2>
-        <img src={img}/>
-        <div className={style.b}>
+      <div className={style.landing1}>
+        <h2>Bienvenidos</h2>
+        <h1>Food Api Dogs</h1>
+        {/* <img src={img}/> */}
+        {/* <div className={style.b}> */}
         <button className={style.button}>
         <Link to='/home' className={style.but}>Ingresar</Link>
         </button>
-        </div>
+        {/* </div> */}
+      </div>
+      <div className={style.landing2}>
+        <img src={logo}/>
+      </div>
     </div>
   )
 }
